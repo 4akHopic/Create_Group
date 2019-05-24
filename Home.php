6bb4f8c3b9ff34446c6c931d1567de665f47c193
4akHopic <48962971+4akHopic@users.noreply.github.com>
2019-05-24 23:27:55 +0300

    add css
    css

--- Home.php
+++ Home.php
@@ -1,6 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="icon" href="/img/iconff.ico">
+  <title>App</title>
+</head>
+<body>
+  <div class="center-auto">
 <form action="redirect.php" method="POST">
-<input type="submit" name="add_group" value="Add group"/>
+<input class="sub" type="submit" name="add_group" value="Add group"/>
 </form>
+</div>
 
 <?php
 
@@ -17,17 +30,20 @@ if($exist->tableExists()){
     $stmt = $pdo->query("SELECT * FROM `$table_alpha`");
    $groupbase = $stmt->fetchAll();
    array_multisort($groupbase, SORT_DESC);
-   $res = '<table>';
+   $res = '<div class="center-auto"><table>';
    foreach ($groupbase as $val) {
      $res .= '<tr>';
-
-     $res .= '<td><form action="redirect.php" method="POST"><input hidden="hidden" type="text" name="name_group" value="'. $val['name'] .'"/><a href="addCommand.php?name='. $val['name'] .'">'.$val['name'].'</a><input type="submit" name="group_id" value="x"/></form></td>';
-
+     $res .= '<td>';
+     $res .= '<form action="redirect.php" method="POST"><input hidden="hidden" type="text" name="name_group" value="'. $val['name'] .'"/></td><td><a href="addCommand.php?name='. $val['name'] .'">'.$val['name'].'</a><input class="sendsubmit" type="submit" name="group_id"/></form>';
+     $res .= '</td>';
      $res .= '</tr>';
    }
 
-   echo $res .= '</table>';
+   echo $res .= '</table></div>';
 ///////////////////////////////////////
 //////////////vuvod table/////////////
 /////////////////////////////////////
 }
+?>
+</body>
+</html>
--- addCommand.php
+++ addCommand.php
@@ -1,3 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="icon" href="/img/iconff.ico">
+  <title>App</title>
+</head>
+<body>
 <?php session_start();
 
 require_once 'DataBase_Connection/PDO_DB_Connect.php';
@@ -14,48 +25,58 @@ session_write_close();
 
 $exist = new Exist($pdo, $table);
 if($exist->tableExists()) {
-  ?>
-  <form action="redirect.php" method="post">
-    <input type='text' required name="team">
-    <input type="submit" name="add_team" value="Add">
-  </form>
 
+  ?>
+
+  <div class="center">
+    <td><a href="Home.php"><img src="img/home.png" alt="Home"></a></td>
+    <div class="center-auto">
+  <table>
+    <tr>
+  <form action="redirect.php" method="post">
+
+    <td><input class="edit" type='text' required name="team"></td>
+    <td><input class="add" type="submit" name="add_team" value="Add"></td>
+  </form>
+</tr>
+</table>
   <?php
+
 ///////////////////////////////////////
 //////////////vuvod table/////////////
 /////////////////////////////////////
 $stmt = $pdo->query("SELECT * FROM $table");
 $teams = $stmt->fetchAll();
 
-$res = '<table>';
+$res = '<table width=100>';
 foreach ($teams as $val) {
   $res .='<tr>';
-  $res .= '<td><form action="redirect.php" method="POST"><input type="hidden" name="team" value="'. $val['team'] .'"/><input type="submit" name="id_addCommand" value="x"/>';
-  $res .= $val['team'] .'</form> <td>';
+  $res .= '<form action="redirect.php" method="POST"><input type="hidden" name="team" value="'. $val['team'] .'"/><td><input class="sendsubmit-visible" type="submit" name="id_addCommand"/></td><td>'. $val['team'] .'</td></form>';
   $res .='</tr>';
 }
 echo $res . '</table>';
+
 ///////////////////////////////////////
 //////////////vuvod table/////////////
 ////////////////////////////////////
 
 $members=$pdo->query("SELECT COUNT(*) as count FROM $table")->fetchColumn();
 
-
 if ($_SESSION["$counters"] < 1 && $members > 1){
 ?>
+
    <form action="redirect.php" method="post">
-     <input  type="submit" name="generate" value="Generate">
+     <input class="add" type="submit" name="generate" value="Generate">
    </form>
 
  <?php
 }else { ?>
+
    <form action="" method="post">
-     <input disabled type="submit" name="generate" value="Generate">
+     <input disabled class="add-disabled" type="submit" name="generate" value="Generate">
    </form>
 
 <?php }
-
 //<-------------------------------------------------------------------------------------------------------------------------------//
 
   $generate = $table . $table;
@@ -68,10 +89,11 @@ if ($_SESSION["$counters"] < 1 && $members > 1){
     /////////////////////////////////////////////////
     $stmt = $pdo->query("SELECT * FROM `$generate`");
    $groupbase = $stmt->fetchAll();
+
    $res = '<table>';
    foreach ($groupbase as $val) {
      $res .= '<tr>';
-     $res .= '<td> <form action="redirect.php" method="POST">' . $val['home'] . '<input type="text" required name="home_rez" value="'.$val['home_rez'].'">' . $val['away'] .'<input type="text" required name="away_rez" value="'.$val['away_rez'].'"><input type="hidden" name="id_rez" value="'. $val['id_rez'] .'"/><input type="submit" name="update_rez" value="UPdate"></form></td>';
+     $res .= '<form action="redirect.php" method="POST"><td>' . $val['home'] . '</td><td><input class="item" type="text" required name="home_rez" value="'.$val['home_rez'] .'"></td><td>'." : ".'</td><td><input class="item" type="text" required name="away_rez" value="'.$val['away_rez'].'"></td><td>' . $val['away'] .'</td><input type="hidden" name="id_rez" value="'. $val['id_rez'] .'"/><td><input class="add" type="submit" name="update_rez" value="UPdate"></td></form>';
      $res .= '</tr>';
    }
    echo $res .= '</table>';
@@ -81,7 +103,12 @@ if ($_SESSION["$counters"] < 1 && $members > 1){
    //////////////vuvod table/////////////
    /////////////////////////////////////
    /////////////////////////////////////////////////
+
+echo '</div>';
  }else {
    echo 'Page not found  <a href="Home.php">Home</a>';
  }
 ?>
+
+</body>
+</html>
